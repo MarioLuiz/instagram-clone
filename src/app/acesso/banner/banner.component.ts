e7e784ed90d0331e2179465ef1dc56f743fda4e8
MarioLuiz <marioluiz.arruda@gmail.com>
2021-05-12 20:44:21 -0400

    Adicionando animação ativa ao banner

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,15 +1,33 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'instaclone-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
+  animations: [
+    trigger('banner', [
+      state('escondido', style({
+        opacity: 0
+      })),
+      state('visivel', style({
+        opacity: 1
+      })),
+      transition('escondido <=> visivel', animate('2s ease-in'))
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
+
+  public estado: string = 'visivel'
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleEstado(): void {
+    this.estado = this.estado === 'visivel' ? 'escondido' : 'visivel'
   }
 
 }
