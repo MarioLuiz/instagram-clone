df55a994d4b79b4ec026704aa4af2e9a60448270
MarioLuiz <marioluiz.arruda@gmail.com>
2021-05-12 21:39:14 -0400

    Implementando lógica de transição de imagens na animação

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,5 +1,7 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { Imagem } from './imagem.model';
+
 
 @Component({
   selector: 'instaclone-banner',
@@ -20,14 +22,35 @@ import { Component, OnInit } from '@angular/core';
 export class BannerComponent implements OnInit {
 
   public estado: string = 'visivel'
-
+  public imagens: Imagem[] = [
+    { estado: 'visivel', url: '/assets/banner-acesso/img_1.png' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_2.png' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_3.png' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_4.png' },
+    { estado: 'escondido', url: '/assets/banner-acesso/img_5.png' }
+  ]
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => this.logicaRotacao(), 4000)
   }
 
-  toggleEstado(): void {
-    this.estado = this.estado === 'visivel' ? 'escondido' : 'visivel'
-  }
+  public logicaRotacao() {
+    //auxilia na exibição da imagem seguinte
+    let idx: number = 0
 
+    // ocultar imagem
+    for (let i = 0; i <= (this.imagens.length - 1); i++) {
+      if (this.imagens[i].estado === 'visivel') {
+        this.imagens[i].estado = 'escondido'
+        idx = i === 4 ? 0 : (i + 1)
+        break
+      }
+    }
+
+    // exibir a proxima imagem
+    this.imagens[idx].estado = 'visivel'
+
+    setTimeout(() => this.logicaRotacao(), 4000)
+  }
 }
