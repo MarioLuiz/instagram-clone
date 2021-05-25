bea8a76dd3a26350c13d82251b1f14f547a9b621
MarioLuiz <marioluiz.arruda@gmail.com>
2021-05-25 18:10:28 -0400

    Adicionando config iniciais do SDK firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import firebase from 'firebase';
+
 
 @Component({
   selector: 'instaclone-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'instagram-clone';
+
+  ngOnInit(): void {
+
+    const firebaseConfig = {
+      apiKey: "", // colocar conforme as configurações do firebase
+      authDomain: "", // colocar conforme as configurações do firebase
+      projectId: "", // colocar conforme as configurações do firebase
+      storageBucket: "", // colocar conforme as configurações do firebase
+      messagingSenderId: "", // colocar conforme as configurações do firebase
+      appId: "", // colocar conforme as configurações do firebase
+      measurementId: "" // colocar conforme as configurações do firebase
+    };
+    firebase.initializeApp(firebaseConfig)
+  }
 }
