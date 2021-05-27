553c429dde1105d47758c0448822d4eea7258b60
MarioLuiz <marioluiz.arruda@gmail.com>
2021-05-26 22:09:14 -0400

    Adicionado HomeComponent e PublicacoesComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { AcessoComponent } from './acesso/acesso.component';
 import { BannerComponent } from './acesso/banner/banner.component';
 import { LoginComponent } from './acesso/login/login.component';
 import { CadastroComponent } from './acesso/cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
+import { PublicacoesComponent } from './home/publicacoes/publicacoes.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +21,8 @@ import { CadastroComponent } from './acesso/cadastro/cadastro.component';
     BannerComponent,
     LoginComponent,
     CadastroComponent,
+    HomeComponent,
+    PublicacoesComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/publicacoes/publicacoes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'instaclone-publicacoes',
+  templateUrl: './publicacoes.component.html',
+  styleUrls: ['./publicacoes.component.scss']
+})
+export class PublicacoesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
